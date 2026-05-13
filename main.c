ef5ccfbaafc7d7abbdcaac126d94bebfc15b0ef4
Hyun Jae Cho <chohyunjae163@gmail.com>
2026-05-14 00:03:22 +0900

    adding rope data structure info

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 // this is the grand beginning of my editor!!
 // input: characters
 // put them into a data structure ( possibly a rope ? )
-
+// about rope :
+//  - https://zed.dev/blog/zed-decoded-rope-sumtree
+//  - 
 int main(void) {
 
   return 0;
